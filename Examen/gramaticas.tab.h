a6759fe7a0b879fdccf074141c280435b3af34f5
Ana Laura <analaura19mongesoto@gmail.com>
2018-07-04 04:22:36 -0600

    Backup

---
+++ Examen/gramaticas.tab.h
@@ -0,0 +1,120 @@
+/* A Bison parser, made by GNU Bison 2.7.  */
+
+/* Bison interface for Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989-1990, 2000-2012 Free Software Foundation, Inc.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+   
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_YY_GRAMATICAS_TAB_H_INCLUDED
+# define YY_YY_GRAMATICAS_TAB_H_INCLUDED
+/* Enabling traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+/* "%code requires" blocks.  */
+/* Line 2058 of yacc.c  */
+#line 583 "gramaticas.cpp"
+
+	#include <list>
+	#include <string>
+	using namespace std;
+
+
+/* Line 2058 of yacc.c  */
+#line 53 "gramaticas.tab.h"
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     ID = 258,
+     PYC = 259,
+     FOR = 260,
+     PARD = 261,
+     CORD = 262,
+     PARI = 263,
+     CORI = 264,
+     IGUAL = 265,
+     COM = 266,
+     MAS = 267,
+     MENOS = 268,
+     ERROR = 269,
+     DOSP = 270,
+     NUM = 271,
+     COMPARACION = 272,
+     RET = 273,
+     PRINT = 274,
+     INI = 275,
+     FIN = 276
+   };
+#endif
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE
+{
+/* Line 2058 of yacc.c  */
+#line 588 "gramaticas.cpp"
+
+	string* hilera;
+	int intVal;
+	struct Caja* nodo;
+	struct Simbolos* simbolo;
+	std::list<Caja*>* params;
+
+
+/* Line 2058 of yacc.c  */
+#line 98 "gramaticas.tab.h"
+} YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+extern YYSTYPE yylval;
+
+#ifdef YYPARSE_PARAM
+#if defined __STDC__ || defined __cplusplus
+int yyparse (void *YYPARSE_PARAM);
+#else
+int yyparse ();
+#endif
+#else /* ! YYPARSE_PARAM */
+#if defined __STDC__ || defined __cplusplus
+int yyparse (void);
+#else
+int yyparse ();
+#endif
+#endif /* ! YYPARSE_PARAM */
+
+#endif /* !YY_YY_GRAMATICAS_TAB_H_INCLUDED  */
